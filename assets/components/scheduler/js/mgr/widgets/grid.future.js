342b9c893f0b220ecd5ce4800f1a3c569208ba36
Bert Oost <bert@oostdesign.nl>
2014-05-02 09:50:52 +0200

    Spiced up fields definition in tasks grid

--- assets/components/scheduler/js/mgr/widgets/grid.future.js
+++ assets/components/scheduler/js/mgr/widgets/grid.future.js
@@ -1,72 +1,70 @@
 Scheduler.grid.Future = function(config) {
     config = config || {};
-    var exp = new Ext.grid.RowExpander({
-        tpl : new Ext.Template('<p>{task_description}</p> {data}')
-    });
     Ext.applyIf(config,{
-		id: 'scheduler-grid-future',
-        baseParams: {
-            action: 'mgr/runs/future'
-        },
-        emptyText: _('scheduler.error.noresults'),
-        plugins: exp,
-		columns: [exp, {
-			header: _('id'),
-			dataIndex: 'id',
-			sortable: true,
-			width: 5,
-            hidden: true
+		id: 'scheduler-grid-future'
+        ,baseParams: { action: 'mgr/runs/future' }
+        ,emptyText: _('scheduler.error.noresults')
+        ,fields: [
+            { name: 'id', type: 'int' }
+            ,{ name: 'status', type: 'string' }
+            ,{ name: 'task', type: 'string' }
+            ,{ name: 'timing', type: 'date', dateFormat: 'U' }
+            ,{ name: 'data', type: 'string' }
+            ,{ name: 'executedon', type: 'date', dateFormat: 'U' }
+            ,{ name: 'errors', type: 'string' }
+            ,{ name: 'message', type: 'string' }
+        ]
+        ,paging: true
+		,remoteSort: true
+        ,plugins: this.exp
+		,columns: [this.exp, {
+			header: _('id')
+			,dataIndex: 'id'
+			,sortable: true
+			,width: 5
+            ,hidden: true
 		},{
-			header: _('scheduler.task'),
-			dataIndex: 'task',
-			sortable: true,
-			width: 5,
-            hidden: true
+			header: _('scheduler.task')
+			,dataIndex: 'task'
+			,sortable: true
+			,width: 5
+            ,hidden: true
 		},{
-			header: _('scheduler.namespace'),
-			dataIndex: 'task_namespace',
-		    sortable: true,
-			width: 15
+			header: _('scheduler.namespace')
+			,dataIndex: 'task_namespace'
+		    ,sortable: true
+			,width: 15
 		},{
-			header: _('scheduler.reference'),
-			dataIndex: 'task_reference',
-		    sortable: true,
-			width: 15
+			header: _('scheduler.reference')
+			,dataIndex: 'task_reference'
+		    ,sortable: true
+			,width: 15
 		},{
-			header: _('scheduler.timing'),
-			dataIndex: 'timing',
-		    sortable: true,
-			width: 15,
-            renderer: Ext.util.Format.dateRenderer('Y-m-d H:i')
+			header: _('scheduler.timing')
+			,dataIndex: 'timing'
+		    ,sortable: true
+			,width: 15
+            ,renderer: Ext.util.Format.dateRenderer('Y-m-d H:i')
 		},{
-			header: _('scheduler.content'),
-			dataIndex: 'task_content',
-		    sortable: true,
-			width: 25,
-            hidden: true
+			header: _('scheduler.content')
+			,dataIndex: 'task_content'
+		    ,sortable: true
+			,width: 25
+            ,hidden: true
 		},{
-			header: _('scheduler.data'),
-			dataIndex: 'data',
-		    sortable: false,
-			width: 25
-		}],
-        tbar: []
+			header: _('scheduler.data')
+			,dataIndex: 'data'
+		    ,sortable: false
+			,width: 25
+		}]
+        ,tbar: []
     });
     Scheduler.grid.Future.superclass.constructor.call(this,config);
 };
 Ext.extend(Scheduler.grid.Future,Scheduler.grid.History,{
-    statusRenderer: function(value) {
-        var v = _('scheduler.status_' + value);
-        if (value == 2) {
-            v = '<span style="color:green;">'+v+'</span>';
-        }
-        if (value == 3) {
-            v = '<span style="color:red;">'+v+'</span>';
-        }
-        return v;
-    },
+    exp: new Ext.grid.RowExpander({ tpl : new Ext.Template('<p>{task_description}</p> {data}') })
 
-    getMenu: function() {
+    ,getMenu: function() {
         var m = [];
         return m;
     }
