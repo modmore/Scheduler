470e3b5a0ee256fa06cb073240760fd6e9fd187b
Bert Oost <bert@oostdesign.nl>
2014-05-02 10:19:46 +0200

    Updated scheduled tasks grid with create, update, remove and filter options

--- assets/components/scheduler/js/mgr/widgets/grid.future.js
+++ assets/components/scheduler/js/mgr/widgets/grid.future.js
@@ -8,6 +8,9 @@ Scheduler.grid.Future = function(config) {
             { name: 'id', type: 'int' }
             ,{ name: 'status', type: 'string' }
             ,{ name: 'task', type: 'string' }
+            ,{ name: 'task_namespace', type: 'string' }
+            ,{ name: 'task_reference', type: 'string' }
+            ,{ name: 'task_content', type: 'string' }
             ,{ name: 'timing', type: 'date', dateFormat: 'U' }
             ,{ name: 'data', type: 'string' }
             ,{ name: 'executedon', type: 'date', dateFormat: 'U' }
@@ -57,16 +60,108 @@ Scheduler.grid.Future = function(config) {
 		    ,sortable: false
 			,width: 25
 		}]
-        ,tbar: []
+        ,tbar: [{
+            text: _('scheduler.run_create')
+            ,handler: {
+                xtype: 'scheduler-window-run-createupdate'
+                ,blankValues: true
+            }
+        },'->',{
+            xtype: 'modx-combo-namespace'
+            ,id: 'scheduler-filter-run-namespace'
+            ,emptyText: _('scheduler.filter_namespace...')
+            ,allowBlank: true
+            ,listeners: {
+                'select': { fn: this.searchNamespace ,scope: this }
+            }
+        },'-',{
+            xtype: 'textfield'
+            ,id: 'scheduler-search-run-field'
+            ,emptyText: _('scheduler.search...')
+            ,listeners: {
+                'change': { fn: this.search ,scope: this }
+                ,'render': { fn: function(cmp) {
+                    new Ext.KeyMap(cmp.getEl(), {
+                        key: Ext.EventObject.ENTER
+                        ,fn: function() {
+                            this.fireEvent('change',this);
+                            this.blur();
+                            return true;
+                        }
+                        ,scope: cmp
+                    });
+                } ,scope: this }
+            }
+        },'-',{
+            text: _('scheduler.search_clear')
+            ,handler: this.searchClear
+        }]
     });
     Scheduler.grid.Future.superclass.constructor.call(this,config);
 };
 Ext.extend(Scheduler.grid.Future,Scheduler.grid.History,{
     exp: new Ext.grid.RowExpander({ tpl : new Ext.Template('<p>{task_description}</p> {data}') })
+    ,search: function(tf,nv,ov) {
+        var s = this.getStore();
+            s.baseParams.query = tf.getValue();
+        this.getBottomToolbar().changePage(1);
+        this.refresh();
+    }
+    ,searchNamespace: function(tf,nv,ov) {
+        var s = this.getStore();
+            s.baseParams.namespace = tf.getValue();
+        this.getBottomToolbar().changePage(1);
+        this.refresh();
+    }
+    ,searchClear: function(tf,nv,ov) {
+        var s = this.getStore();
+            s.baseParams.query = '';
+            s.baseParams.class_key = '';
+        this.getBottomToolbar().changePage(1);
+        this.refresh();
 
+        Ext.getCmp('scheduler-filter-run-namespace').reset();
+        Ext.getCmp('scheduler-search-run-field').reset();
+    }
     ,getMenu: function() {
-        var m = [];
+        var m = [{
+            text: _('scheduler.run_update')
+            ,handler: this.updateRun
+        },'-',{
+            text: _('scheduler.run_remove')
+            ,handler: this.removeRun
+        }];
         return m;
+    }
+    ,updateRun: function(btn, e) {
+        var w = MODx.load({
+			xtype: 'scheduler-window-run-createupdate'
+            ,isUpdate: true
+			,record: this.menu.record
+			,listeners: {
+				'success': { fn: this.refresh, scope: this }
+				,'hide': { fn: function() { this.destroy(); }}
+			}
+		});
+		w.setTitle(_('scheduler.run_update'));
+		w.setValues(this.menu.record);
+		w.show(e.target, function() {
+			Ext.isSafari ? w.setPosition(null,30) : w.center();
+		}, this);
+    }
+    ,removeRun: function(btn, e) {
+        MODx.msg.confirm({
+			title: _('scheduler.run_remove')
+			,text: _('scheduler.run_remove_confirm', { reference: this.menu.record.reference })
+			,url: this.config.url
+			,params: {
+				action: 'mgr/runs/remove'
+				,id: this.menu.record.id
+			}
+			,listeners: {
+				'success': { fn:this.refresh, scope:this }
+			}
+		});
     }
 });
 Ext.reg('scheduler-grid-future',Scheduler.grid.Future);
