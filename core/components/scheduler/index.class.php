f6d3e080ef95432da4916d1581e02f80bba3c4e2
Nikolay Savin <biz87@mail.ru>
2025-01-21 00:17:57 +0500

    fix some errors for modx3 init

--- core/components/scheduler/index.class.php
+++ core/components/scheduler/index.class.php
@@ -3,24 +3,41 @@
 /**
  * The main Scheduler Manager Controller.
  */
-abstract class SchedulerManagerController extends modExtraManagerController {
+abstract class SchedulerManagerController extends modExtraManagerController
+{
     /** @var Scheduler $scheduler */
     public $scheduler = null;
 
     /**
      * Initializes the main manager controller.
      */
-    public function initialize() {
+    public function initialize()
+    {
         /* Instantiate the Scheduler class in the controller */
-        $path = $this->modx->getOption('scheduler.core_path', null, $this->modx->getOption('core_path').'components/scheduler/') . 'model/scheduler/';
-        $this->scheduler = $this->modx->getService('scheduler', 'Scheduler', $path);
+        switch ($this->modx->version['version']) {
+            case 2:
+                $path = $this->modx->getOption('scheduler.core_path', null, $this->modx->getOption('core_path') . 'components/scheduler/') . 'model/scheduler/';
+                $this->scheduler = $this->modx->getService('scheduler', 'Scheduler', $path);
+                break;
+            case 3:
+                if ($this->modx->services->has('scheduler')) {
+                    try {
+                        $this->scheduler = $this->modx->services->get('scheduler');
+                    } catch (\Psr\Container\NotFoundExceptionInterface $e) {
+                        $this->modx->log(1, $e->getMessage());
+                    } catch (\Psr\Container\ContainerExceptionInterface $e) {
+                        $this->modx->log(1, $e->getMessage());
+                    }
+                }
+                break;
+        }
 
         /* Add the main javascript class and our configuration */
-        $this->addJavascript($this->scheduler->config['jsUrl'].'mgr/scheduler.class.js');
-        $this->addJavascript($this->scheduler->config['jsUrl'].'mgr/combos.js');
+        $this->addJavascript($this->scheduler->config['jsUrl'] . 'mgr/scheduler.class.js');
+        $this->addJavascript($this->scheduler->config['jsUrl'] . 'mgr/combos.js');
         $this->addHtml('<script type="text/javascript">
         Ext.onReady(function() {
-            Scheduler.config = '.$this->modx->toJSON($this->scheduler->config).';
+            Scheduler.config = ' . $this->modx->toJSON($this->scheduler->config) . ';
         });
         </script>');
     }
@@ -29,8 +46,9 @@ abstract class SchedulerManagerController extends modExtraManagerController {
      * Defines the lexicon topics to load in our controller.
      * @return array
      */
-    public function getLanguageTopics() {
-        return array('scheduler:default');
+    public function getLanguageTopics()
+    {
+        return ['scheduler:default'];
     }
 
     /**
@@ -38,7 +56,8 @@ abstract class SchedulerManagerController extends modExtraManagerController {
      * controller. We'll apply this in the admin section.
      * @return bool
      */
-    public function checkPermissions() {
+    public function checkPermissions()
+    {
         return true;
     }
 
@@ -46,8 +65,9 @@ abstract class SchedulerManagerController extends modExtraManagerController {
      * The name for the template file to load.
      * @return string
      */
-    public function getTemplateFile() {
-        return $this->scheduler->config['templatesPath'].'mgr.tpl';
+    public function getTemplateFile()
+    {
+        return $this->scheduler->config['templatesPath'] . 'mgr.tpl';
     }
 }
 
@@ -55,12 +75,14 @@ abstract class SchedulerManagerController extends modExtraManagerController {
  * The Index Manager Controller is the default one that gets called when no
  * action is present.
  */
-class ControllersIndexManagerController extends SchedulerManagerController {
+class ControllersIndexManagerController extends SchedulerManagerController
+{
     /**
      * Defines the name or path to the default controller to load.
      * @return string
      */
-    public static function getDefaultController() {
+    public static function getDefaultController()
+    {
         return 'index';
     }
 }
--- core/components/scheduler/src/Model/metadata.mysql.php
+++ core/components/scheduler/src/Model/metadata.mysql.php
@@ -1,15 +1,20 @@
 <?php
 
 $xpdo_meta_map = [
-    'xPDO\\Om\\xPDOSimpleObject' =>
-        [
-            'Scheduler\\Model\\sTask',
-            'Scheduler\\Model\\sTaskRun',
-        ],
-    'Scheduler\\Model\\sTask' =>
-        [
-            'Scheduler\\Model\\sSnippetTask',
-            'Scheduler\\Model\\sProcessorTask',
-            'Scheduler\\Model\\sFileTask',
-        ],
+    'version' => '3.0',
+    'namespace' => 'Scheduler\\Model',
+    'namespacePrefix' => 'Scheduler',
+    'class_map' => [
+        'xPDO\\Om\\xPDOSimpleObject' =>
+            [
+                'Scheduler\\Model\\sTask',
+                'Scheduler\\Model\\sTaskRun',
+            ],
+        'Scheduler\\Model\\sTask' =>
+            [
+                'Scheduler\\Model\\sSnippetTask',
+                'Scheduler\\Model\\sProcessorTask',
+                'Scheduler\\Model\\sFileTask',
+            ],
+    ],
 ];
