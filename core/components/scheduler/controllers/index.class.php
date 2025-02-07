2abe3eb77eca7dcc9ae138e6da16d9d7d756fff4
Murray Wood <murray@digitalpenguin.studio>
2025-02-07 17:59:31 +0800

    Copy and paste from #30

--- core/components/scheduler/controllers/index.class.php
+++ core/components/scheduler/controllers/index.class.php
@@ -4,23 +4,28 @@ require_once dirname(__DIR__) . '/index.class.php';
 /**
  * Scheduler Home controller
  */
-class SchedulerIndexManagerController extends SchedulerManagerController {
+class SchedulerIndexManagerController extends SchedulerManagerController
+{
     /**
      * The pagetitle to put in the <title> attribute.
      * @return null|string
      */
-    public function getPageTitle() {
+    public function getPageTitle()
+    {
         return $this->modx->lexicon('scheduler');
     }
 
     /**
      * Register all the needed javascript files.
      */
-    public function loadCustomCssJs() {
+    public function loadCustomCssJs()
+    {
         $this->addCss($this->scheduler->config['cssUrl'].'mgr.css');
+        $this->addCss($this->scheduler->config['cssUrl'] . 'bootstrap.buttons.css');
         $this->addJavascript($this->scheduler->config['managerUrl'].'assets/modext/widgets/core/modx.grid.local.property.js');
         $this->addJavascript($this->scheduler->config['managerUrl'].'assets/modext/util/datetime.js');
 
+        $this->addJavascript($this->scheduler->config['jsUrl'] . 'mgr/misc/scheduler.utils.js');
         $this->addJavascript($this->scheduler->config['jsUrl'].'mgr/widgets/windows.tasks.js');
         $this->addJavascript($this->scheduler->config['jsUrl'].'mgr/widgets/windows.future.js');
         $this->addJavascript($this->scheduler->config['jsUrl'].'mgr/widgets/windows.history.js');
@@ -28,7 +33,7 @@ class SchedulerIndexManagerController extends SchedulerManagerController {
         $this->addJavascript($this->scheduler->config['jsUrl'].'mgr/widgets/grid.future.js');
         $this->addJavascript($this->scheduler->config['jsUrl'].'mgr/widgets/grid.history.js');
 
-        $this->addJavascript($this->scheduler->config['jsUrl'].'mgr/panels/home.js');
-        $this->addLastJavascript($this->scheduler->config['jsUrl'].'mgr/sections/home.js');
+        $this->addJavascript($this->scheduler->config['jsUrl'] . 'mgr/panels/home.js');
+        $this->addLastJavascript($this->scheduler->config['jsUrl'] . 'mgr/sections/home.js');
     }
 }
